35dd2fe9200d23a0aee365945a88507c9a8de1aa
robertmartua <martua.rumahorbo@gmail.com>
2019-11-20 17:48:20 +0700

    menambah list

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 <body>
     <h1>hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At vel voluptate quod ut obcaecati fugit odit impedit sint autem temporibus rem recusandae a porro harum exercitationem perspiciatis eos perferendis, tenetur in. Odit, sapiente repellendus animi amet sequi voluptatibus minus et iste ducimus magnam delectus cumque recusandae? Sed veritatis deserunt, magni commodi ad, alias tenetur temporibus aspernatur voluptatum corporis voluptatem libero, provident totam odit quas a ut tempore cupiditate! Beatae temporibus iure aut eum minima sunt nostrum accusamus ratione libero enim provident, laborum voluptate ullam consequuntur, aspernatur laudantium placeat cum molestias architecto nulla. Mollitia sit ut libero saepe. Dicta, cupiditate porro!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+
 </body>
 
 </html>
